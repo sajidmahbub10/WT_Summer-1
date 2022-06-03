6c2e0a065aee3695695fa018f1b47fca28f0bdd1
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2022-06-03 14:53:40 +0600

    Add files via upload

---
+++ Lab Task - 2/Control/tasktwo.php
@@ -0,0 +1,110 @@
+<?php
+if (isset($_POST['Submit']))
+{
+    $fname = $_REQUEST['FirstName'];
+    if (empty($fname))
+    {
+        echo 'Please Enter Your First Name';
+    }
+    else 
+    {
+        echo 'Your First Name is ' . $fname;
+    }
+    echo '<br>';
+
+    $lname = $_REQUEST['LastName'];
+    if (empty($lname)) 
+    {
+        echo 'Please Enter Your Last Name';
+    } 
+    else
+    {
+        echo 'Your Last Name is ' . $lname;
+    }
+
+    echo '<br>';
+
+    $age = $_REQUEST['Age'];
+
+     if ($age == '') 
+    {
+        echo 'Please Enter Your Age ';
+    } 
+    else 
+    {
+        echo 'Your Age is ' . $age;
+    }
+
+    echo '<br>';
+    
+    if ( isset($_POST['Desgination1']) || isset($_POST['Desgination2']) || isset($_POST['Desgination3'])) 
+    {
+        if (isset($_POST['Desgination1'])) 
+        {
+            echo 'You Have Been Selected As Junior Programmer ';
+            echo '<br>';
+        }
+        if (isset($_POST['Desgination2'])) 
+        {
+            echo 'You Have Been Selected As Senior Programmer';
+            echo '<br>';
+        }
+        if (isset($_POST['Desgination3'])) 
+        {
+            echo 'You Have been Selected As Project Lead';
+            echo '<br>';
+        }
+    } 
+    else 
+    {
+        echo 'You Have not selected For Anything';
+    }
+
+    echo '<br>';
+
+    
+    if (isset($_POST['PreferredLanguage1']) || isset($_POST['PreferredLanguage2']) || isset($_POST['PreferredLanguage3'])) 
+    {
+        if (isset($_POST['PreferredLanguage1'])) 
+        {
+            echo 'You Have Selected JAVA ';
+            echo '<br>';
+        }
+        else if (isset($_POST['PreferredLanguage2'])) 
+        {
+            echo 'You Have Selected PHP';
+            echo '<br>';
+        }
+        else if (isset($_POST['PreferredLanguage3'])) 
+        {
+            echo 'You Have Selected C++';
+            echo '<br>';
+        }
+    }
+    else 
+    {
+        echo 'You Have not Clicked a checkbox';
+        echo '<br>';
+    }
+
+    $email = $_POST['Email'];
+    if (empty($email) || !preg_match('/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',$email)) 
+    {
+        echo 'Invalid Email';
+        echo '<br>';
+    } 
+    else 
+    {
+        echo 'Your Email Address is ' . $email . '<br>';
+    }
+
+    $password = $_REQUEST['Password'];
+    if (strlen($password) < 8) 
+    {
+        echo 'Enter a Valid password';
+    } 
+    else
+    {
+        echo 'Password is Valid';
+    }
+}
---
+++ Lab Task - 2/View/tasktwo.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration form</title>
+</head>
+
+<body>
+    <h1>Registration Form</h1>
+    <form action="../Control/tasktwo.php" method="post">
+        <table>
+            <tr>
+                <td>First Name : <input type= "text" name ="fname" placeholder =" Enter First Name">  <br></td>
+            </tr>
+
+            <tr>
+                <td>Last Name : <input type ="text" name ="lname" placeholder = "Enter Last Name"><br></td>
+            </tr>
+
+            <tr>
+                 <td>Age : <input type ="text" name ="age" placeholder = "Enter Your Age"><br></td>
+            </tr>
+
+            <tr>
+                <td>
+                    Designation : <input type = "radio" name = "designation1" value ="juniorprogrammer">Junior Programmer
+                    <input type = "radio" name = "designation2" value ="seniorprogrammer">Senior Programmer
+                    <input type = "radio" name = "designation3" value ="projectlead">Project Lead <br>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    Preferred Language : <name="PreferredLanguage">            
+                    <input type="checkbox" name="PreferredLanguage1" >JAVA
+                    <input type="checkbox" name="PreferredLanguage2"> PHP
+                    <input type="checkbox" name="PreferredLanguage3"> C++
+                </td>
+            </tr>
+
+            <tr>
+                <td>Email : <input type ="email" name = "email" placeholder = "Enter Your Email" autocomplete ="on"></td>
+            </tr>
+
+            <tr>
+                <td>Password : <input type ="password" name ="pass" placeholder = "Enter Desired Password"><br></td>
+            </tr>
+
+            <tr>
+                <td>Please choose a file<input type="file"><br></td>
+            </tr>
+
+            <tr>
+                <td>
+                    <input type ="submit"  value="submit">
+                    <input type = "reset">
+                </td>
+            </tr>
+        </table>
+</body>
+
+</html>
