906b1eee7357585200b5f4575e5595d3eab50c26
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-06-06 10:39:29 +0600

    renaming

---
+++ Jsonn/View/two.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+</head>
+<body>
+    <h1>Regiatration form</h1>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>First Name: <input type="text" name="fname" placeholder="enter your first name"></td>
+            </tr>
+
+            <tr>
+                <td>Last Name: <input type="text" name="lname" placeholder="enter your last name"><br></td>
+            </tr>
+
+            <tr>
+                <td>Email: <input type="email" name="email" placeholder="enter email" autocomplete="on"></td>
+            </tr>
+
+            <tr>
+                <td>Password: <input type="password" name="pass" placeholder="enter password"></td>
+            </tr>
+        </table>
+    </form>
+
+</body>
+</html>
