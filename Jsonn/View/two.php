a290650c4d917b3f4fc4670f6a00df1f6030c00a
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-06-08 00:13:34 +0600

    Update two.php

--- Jsonn/View/two.php
+++ Jsonn/View/two.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration From</title>
 </head>
+
 <body>
-    <form action="../Control/process.php" method="POST">
+    <form action="" method="POST">
         First name: <br>
         <input type="text" name="firstName">
         <br><br>
@@ -24,7 +26,8 @@
         <input type="text" name="mobile">
         <br><br>
 
-        <input type="submit" value="submit">
+        <input type="submit" value="submit" name="submit">
     </form>
 </body>
+
 </html>
