0b8ca13e00158ed023a8c7c2a8e2f6df32ba6b2f
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-06-08 00:09:15 +0600

    Update process.php

--- Jsonn/Control/process.php
+++ Jsonn/Control/process.php
@@ -2,28 +2,21 @@
 
 if(isset($_POST['submit']))
 {
-	
-}
-	$fordata = array
-	(
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$email = $_POST['email'];
+	$mobile = $_POST['mobile'];
+
+	$data = file_get_contents('../Data/data.json');
+	$data_array = json_decode($data, true);
+
+	$fordata = array(
 		'firstName' => $_POST["firstName"],
 		'lastName' => $_POST["lastName"],
 		'email'  => $_POST["email"],
 		'mobile' => $_POST["mobile"]
 	);
 
-	$existingdata = file_get_contents("../Data/data.json");
-	$tempJSONdata = json_decode($existingdata);
-	$tempJSONdata[] = $fordata;
-
-	$jsondata = json_encode($tempJSONdata, JSON_PRETTY_PRINT);
-
-	if(file_put_contents("../Data/data.json", $jsondata))
-	{
-		echo "Data Successfully saved <br>";
-	}
-	else
-	{
-		echo "No data Saved";
-	}
+	
+}
 ?>
