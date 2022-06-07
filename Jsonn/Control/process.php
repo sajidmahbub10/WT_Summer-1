21168c8abe736539bedc8386a6ddec1e6ae6cf77
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-06-07 15:59:54 +0600

    update

--- Jsonn/Control/process.php
+++ Jsonn/Control/process.php
@@ -1,27 +1,40 @@
 <?php
-$formdata = array(
-    'firstname' => $_POST["firstname"],
-    'lastname' => $_POST["lastname"],
-    'email' => $_POST["email"],
-    'password' => $_POST["password"]
-);
-
-$existingdata = file_get_contents('../Data/data.json');
-
-$tempJSONdara = json_decode($existingdata);
-$tempJSONdara[] = $formdata;
-$jsondata = json_encode($existingdata, JSON_PRETTY_PRINT);
-
-if (file_put_contents("../Data/data.json", $jsondata)) {
-    echo "Data Successfully Saved <br>";
-} else {
-    echo "no data saved";
-}
-$str_data = file_get_contents("../Data/data.json");
-$data = json_decode($str_data, true);
-foreach ($data as $mydata) {
-    foreach ($mydata as $key => $value) {
-        echo $key . " is " . $value . "<br>";
-    }
+$message = '';
+$error = '';
+if (isset($_POST["submit"])) {
+	if (empty($_POST["name"])) {
+		$error = "<label class='text-danger'>Enter Name</label>";
+	} else if (empty($_POST["email"])) {
+		$error = "<label class='text-danger'>Enter an Email</label>";
+	} else if (empty($_POST["un"])) {
+		$error = "<label class='text-danger'>Enter a User Name</label>";
+	} else if (empty($_POST["pass"])) {
+		$error = "<label class='text-danger'>Enter a Password</label>";
+	} else if (empty($_POST["Cpass"])) {
+		$error = "<label class='text-danger'>Confirm Password field cannot be empty</label>";
+	} else if (empty($_POST["gender"])) {
+		$error = "<label class='text-danger'>Gender cannot be empty</label>";
+	} else {
+		if (file_exists('../Data/data.json')) {
+			$current_data = file_get_contents('../Data/data.json');
+			//echo $current_data;
+			$array_data = json_decode($current_data, true);
+			$new_data = array(
+				'name'               =>     $_POST['name'],
+				'e-mail'          =>     $_POST["email"],
+				'username'     =>     $_POST["un"],
+				'gender'     =>     $_POST["gender"],
+				'dob'     =>     $_POST["dob"]
+			);
+			$array_data[] = $new_data;
+			$final_data = json_encode($array_data);
+			//echo $final_data;
+			if (file_put_contents('../Data/data.json', $final_data)) {
+				$message = "<label class='text-success'>File Save Successfully</p>";
+			}
+		} else {
+			$error = 'JSON File not exits';
+		}
+	}
 }
 ?>
--- Jsonn/View/two.php
+++ Jsonn/View/two.php
@@ -1,39 +1,72 @@
-<?php
-    include("../Control/process.php");
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration Form</title>
 </head>
+
 <body>
-    <h1>Regiatration form</h1>
-    <form action="" method="post">
-        <table>
-            <tr>
-                <td>First Name: <input type="text" name="firstname" placeholder="enter your first name"></td>
-            </tr>
+    <br />
+    <div class="container" style="width:500px;">
+        <form method="post">
+            <?php
+            if (isset($error)) {
+                echo $error;
+            }
+            ?>
+            <br />
 
-            <tr>
-                <td>Last Name: <input type="text" name="lastname" placeholder="enter your last name"><br></td>
-            </tr>
+            <fieldset>
+                <legend>
+                    <h3>REGISTRATION</h3>
+                </legend>
 
-            <tr>
-                <td>Email: <input type="email" name="email" placeholder="enter email" autocomplete="on"></td>
-            </tr>
+                <label>Name &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp :</label>
+                <input type="text" name="name" class="form-control" /> <br>
+                <hr>
+                <label>Email &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp :</label>
+                <input type="text" name="email" class="form-control" /> <b>i</b><br>
+                <hr>
+                <label>User Name &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp :</label>
+                <input type="text" name="un" class="form-control" /> <br>
+                <hr>
+                <label>Password &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp :</label>
+                <input type="password" name="pass" class="form-control" /> <br>
+                <hr>
+                <label>Confirm Password &nbsp :</label>
+                <input type="password" name="Cpass" class="form-control" /> <br>
+                <hr>
 
-            <tr>
-                <td>Password: <input type="password" name="pass" placeholder="enter password"></td>
-            </tr>
 
-            <tr>
-                <td><input type="submit" name="submit" value="submit"></td>
-            </tr>
-        </table>
-    </form>
+                <fieldset>
+                    <legend>Gender</legend>
+                    <input type="radio" id="male" name="gender" value="male">
+                    <label for="male">Male</label>
+                    <input type="radio" id="female" name="gender" value="female">
+                    <label for="female">Female</label>
+                    <input type="radio" id="other" name="gender" value="other">
+                    <label for="other">Other</label> <br>
+                </fieldset>
+                <hr>
 
+                <fieldset>
+                    <legend>Date of Birth:</legend> <br>
+
+                    <input type="date" name="dob"> <br>
+
+                </fieldset>
+                <hr>
+
+                <input type="submit" name="submit" value="Submit" class="btn btn-info" /> <input type="reset" value="Reset"> <br> <br>
+
+                <?php
+                if (isset($message)) {
+                    echo $message;
+                }
+                ?>
+
+            </fieldset>
+        </form>
+    </div>
 </body>
+
 </html>
