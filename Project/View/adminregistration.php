72f557d482b3d265b4786d93b36ab0163c1fb2ae
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2022-06-14 23:46:59 +0600

    update

---
+++ Project/View/adminregistration.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Registration</title>
+</head>
+
+<body>
+    <center>
+        <h1>Welcome to XYZ Bank Limited.</h1>
+        <p><strong>To do further work, please log in.</strong></p>
+        <br><br><br><br><br><br><br><br>
+
+        <form action="" method="POST" enctype="multipart/form-data">
+            <center>
+                <table>
+                    <tr>
+                        <td>
+                            <label>Firstname: </label>
+                            <input type="text" name="fname">
+                            <br><br>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label>Lastname: </label>
+                            <input type="text" name="lname">
+                            <br><br>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label>Username: </label>
+                            <input type="text" name="uname">
+                            <br><br>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label>Email: </label>
+                            <input type="email">
+                        </td>
+                    </tr>
+                </table>
+            </center>
+        </form>
+    </center>
+</body>
+
+</html>
