d32b7703874dadefe0f609ec1650a7714f509030
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2022-06-14 20:13:51 +0600

    Update process.php

--- Lab Task - 2/Control/process.php
+++ Lab Task - 2/Control/process.php
@@ -1,81 +1,111 @@
 <?php
-$namerr = "";
-if (isset($_POST['submit'])) {
-    $fname = $_REQUEST['fname'];
-    if (empty($fname)) {
-        $namerr = "Please Enter Your First Name";
-    } else {
-        echo 'Your First Name is ' . $fname;
+if (isset($_POST['Submit']))
+
+{
+    $FirsName = $_REQUEST['FirstName'];
+        if (empty($FirsName)) 
+    {
+        echo 'Please Enter a valid First Name';
+    }
+         else 
+    {
+        echo 'Your First Name is ' . $FirsName;
     }
     echo '<br>';
 
-    $lname = $_REQUEST['lname'];
-    if (empty($lname)) {
-        echo 'Please Enter Your Last Name';
-    } else {
-        echo 'Your Last Name is ' . $lname;
-    }
-
-    echo '<br>';
-
-    $age = $_REQUEST['age'];
-
-    if ($age == '') {
-        echo 'Please Enter Your Age ';
-    } else {
-        echo 'Your Age is ' . $age;
+        $LastName = $_REQUEST['LastName'];
+        if (empty($LastName)) 
+    {
+        echo 'Please Enter a valid Last Name';
+    } else
+    {
+        echo 'Your Last Name is ' . $LastName;
     }
 
     echo '<br>';
 
-    if (isset($_POST['Desgination1']) || isset($_POST['Desgination2']) || isset($_POST['Desgination3'])) {
+    $Age = $_REQUEST['Age'];
+
+        if ($Age == '') 
+    {
+        echo 'Please Enter your Age ';
+    } else 
+    {
+        echo 'Your Age is ' . $Age;
+    }
+
+    echo '<br>';
+    
+    if (
+        isset($_POST['Desgination1']) ||    // null kina  isset check kore radio button r error
+        isset($_POST['Desgination2']) ||
+        isset($_POST['Desgination3'])) 
+        {
         if (isset($_POST['Desgination1'])) {
-            echo 'You Have Been Selected As Junior Programmer ';
+            echo 'Your Have Selected Junior Programmer ';
             echo '<br>';
         }
         if (isset($_POST['Desgination2'])) {
-            echo 'You Have Been Selected As Senior Programmer';
+            echo 'Your Have Selected Senior Programmer';
             echo '<br>';
         }
         if (isset($_POST['Desgination3'])) {
-            echo 'You Have been Selected As Project Lead';
+            echo 'Your Have Selected Project Lead';
             echo '<br>';
         }
     } else {
-        echo 'You Have not selected For Anything';
+        echo 'You Have not selected anything';
     }
 
     echo '<br>';
 
-
-    if (isset($_POST['PreferredLanguage1']) || isset($_POST['PreferredLanguage2']) || isset($_POST['PreferredLanguage3'])) {
+    
+    if (
+        isset($_POST['PreferredLanguage1']) ||
+        isset($_POST['PreferredLanguage2']) ||
+        isset($_POST['PreferredLanguage3'])) {
         if (isset($_POST['PreferredLanguage1'])) {
-            echo 'You Have Selected JAVA ';
-            echo '<br>';
-        } else if (isset($_POST['PreferredLanguage2'])) {
-            echo 'You Have Selected PHP';
-            echo '<br>';
-        } else if (isset($_POST['PreferredLanguage3'])) {
-            echo 'You Have Selected C++';
+            echo 'Your Have Selected JAVA ';
             echo '<br>';
         }
-    } else {
-        echo 'You Have not Clicked a checkbox';
+
+        if (isset($_POST['PreferredLanguage2'])) {
+            echo 'Your Have Selected PHP';
+            echo '<br>';
+        }
+        if (isset($_POST['PreferredLanguage3'])) {
+            echo 'Your Have Selected C++';
+            echo '<br>';
+        }
+    } 
+    else {
+        echo 'You Have not Selected a checkbox';
         echo '<br>';
     }
 
-    $email = $_POST['email'];
-    if (empty($email) || !preg_match('/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix', $email)) {
-        echo 'Invalid Email';
+    $Email = $_POST['Email'];
+    if (
+        empty($Email) ||
+        !preg_match(
+            '/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',
+            $Email
+        )
+    ) {
+        echo 'Invalid email';
         echo '<br>';
-    } else {
-        echo 'Your Email Address is ' . $email . '<br>';
+    } else 
+    {
+        echo 'Your email is ' . $Email . '<br>';
     }
 
-    $password = $_REQUEST['pass'];
-    if (strlen($password) < 8) {
-        echo 'Enter a Valid password';
-    } else {
-        echo 'Password is Valid <br>';
+    $Password = $_REQUEST['Password'];
+    if (strlen($Password) < 8) {
+        echo 'Enter a valid password';
+    } else
+     {
+        echo 'password is valid';
     }
+
+
+
 }
