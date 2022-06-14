ab1782d8e10dceb8d52eaba9abade2586e5be904
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2022-06-14 22:26:54 +0600

    Update

--- Project/View/adminlogin.php
+++ Project/View/adminlogin.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Admin Log in</title>
 </head>
+
 <body>
-    
+    <center>
+        <h1>Welcome to XYZ Bank Limited.</h1>
+        <p><strong>To do further work, please log in.</strong></p>
+        <br><br><br><br><br><br><br><br>
+
+        <form action="" method="POST" enctype="multipart/form-data">
+            <center>
+                <table>
+                    <tr>
+                        <td>
+                            <label>Username: </label>
+                            <input type="text" name="uname"> <br><br>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label>Password : </label>
+                            <input type="password" name="pass"> <br><br>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <input type="submit" name="submit" value="Login">
+                            <input type="submit" name="registration" value="Registration">
+                        </td>
+                    </tr>
+                </table>
+            </center>
+        </form>
+    </center>
 </body>
+
 </html>
